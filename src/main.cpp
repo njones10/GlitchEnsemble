734576e505abe39fb53ba383f857c515acc85b18
colejohnsonline <cjohnson13@luc.edu>
2014-02-20 13:25:04 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,5 @@
 
 int main()
 {
-   std::cout << "AuralGlitch is a standalone audio sampler and effects program built in C++.\n";
+   std::cout << "AuralGlitch is a standalone audio sampler and effects program for OS X built in C++.\n";
 }
